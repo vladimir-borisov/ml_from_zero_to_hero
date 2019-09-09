771ffde99a8bcae428cdea7cd13ff2300a8edf48
vu-borisov <vu.borisoff@gmail.com>
2019-09-09 11:31:02 +0300

    Added 2 activation functions

--- neural_networks/activation_functions.py
+++ neural_networks/activation_functions.py
@@ -1,1 +1,17 @@
-#Some activation functions in python will be here
+import numpy as np
+
+'''
+    Linear activation function like (kx + b)
+
+    Params:
+    x = variable
+    k = angle coefficient
+    b = bias coefficient
+'''
+def Linear (x, k, b):
+    return k * x + b
+
+
+def Sigmoid(x):
+    return 1.0 / (1.0 + np.exp(-x))
+
