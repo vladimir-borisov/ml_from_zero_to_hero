8588813393cdb819f70ab9e48731ae33715d904b
v.borisov <v.borisov@haut.ai>
2019-09-13 18:44:42 +0300

    Changes in one layer perceptron

--- neural_networks/one_layer_perceptron.py
+++ neural_networks/one_layer_perceptron.py
@@ -1,19 +1,37 @@
 import numpy as np
+from neural_networks.activation_functions import ReLU, Softmax, ELU
 
 '''
 One layer perceptron: class consist of only 2 layers (input, output)
 '''
 class OneLayerPreceptron:
-    def __init__(self, w_count):
-        self.w = np.random().rand(1, w_count)
-        self.bias = np.zeros(w_count)
+    def __init__(self, input_size, output_size, activation_function = Softmax):
+        #set random weights between [0, 1)
+        self.w = np.random().rand(input_size, output_size)
+        self.bias = np.zeros(output_size)
+
+        #inner variables for outputs and inputs for access after forward and backward operations
+        self.outputs = np.zeros(output_size)
+        self.inputs = np.zeros(input_size)
+
+        #set activation function
+        self.activation_function = activation_function
 
     #forward + backward
-    def fit(self):
+    def fit(self, x, y_true):
         pass
 
-    def backward(self):
-        pass
+    def forward(self, x):
+        weighted_sum = x.dot(self.w * x) + self.bias
+        self.outputs = self.activation_function(weighted_sum)
 
-    def forward(self):
-        pass
+    def predict(self, x):
+        self.forward(x)
+        return self.outputs
+
+
+model = OneLayerPreceptron(4, 2)
+
+#TODO fix error with numpy module
+model.predict([1, 2, 3, 4])
+
