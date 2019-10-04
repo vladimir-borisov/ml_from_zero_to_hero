27d7989ad216d1d161c3d7f03139700edb38f32c
v.borisov <v.borisov@haut.ai>
2019-10-04 18:49:48 +0300

    Fixed single layer perceptron Added some lines for MSE

--- neural_networks/loss_functions.py
+++ neural_networks/loss_functions.py
@@ -1,1 +1,8 @@
-#loss functions will be here
+import numpy as np
+
+class MeanSquaredError:
+    def __call__ (self, predict_labels, true_labels):
+        pass
+
+    def derivative(self, predict_labels, true_labels):
+        pass
--- neural_networks/one_layer_perceptron.py
+++ neural_networks/one_layer_perceptron.py
@@ -5,9 +5,10 @@ from neural_networks.activation_functions import ReLU, Softmax, ELU
 One layer perceptron: class consist of only 2 layers (input, output)
 '''
 class OneLayerPreceptron:
-    def __init__(self, input_size, output_size, activation_function = Softmax):
+    def __init__(self, input_size, output_size, activation_function = Softmax, loss_function = ):
+
         #set random weights between [0, 1)
-        self.w = np.random().rand(input_size, output_size)
+        self.w = np.random.rand(input_size, output_size)
         self.bias = np.zeros(output_size)
 
         #inner variables for outputs and inputs for access after forward and backward operations
@@ -17,21 +18,25 @@ class OneLayerPreceptron:
         #set activation function
         self.activation_function = activation_function
 
+
+
     #forward + backward
     def fit(self, x, y_true):
         pass
 
     def forward(self, x):
-        weighted_sum = x.dot(self.w * x) + self.bias
+        weighted_sum = x.dot(self.w) + self.bias
         self.outputs = self.activation_function(weighted_sum)
+
+    def backward(self, x):
+
 
     def predict(self, x):
         self.forward(x)
         return self.outputs
 
 
-model = OneLayerPreceptron(4, 2)
+model = OneLayerPreceptron(5, 3)
 
-#TODO fix error with numpy module
-model.predict([1, 2, 3, 4])
+print(model.predict(np.array([1, 2, 3, 4, 5])))
 
